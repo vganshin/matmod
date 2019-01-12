3e6a28060651606062330dc24cb23a883cb2de96
Vladislav Ganshin <ganshinv@gmail.com>
2019-01-12 21:19:01 +0300

    Prisoner is ready

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var logFile = `logs/${new Date()}.txt`;
 
 function log(msg) {
-    console.log(msg);
+    // console.log(msg);
     fs.appendFileSync(logFile, `${msg}\n`);
 }
 
@@ -14,34 +14,65 @@ var debug = true;
 
 var connection;
 
+function do_login() {
+    send({state: 'login', login, debug, password});
+}
+
+function send(data) {
+    if (connection === undefined) {
+        log('Connection is undefined');
+    }
+    const data2 = JSON.stringify(data);
+    log('Sending: ' + data2);
+    connection.sendUTF(data2);
+}
+
 var client = new WebSocketClient();
 
 client.on('connectFailed', function(error) {
     log('Connect Error: ' + error.toString());
 });
 
-client.on('connect', function(connection) {
+const games = {};
+
+client.on('connect', function(conn) {
+    connection = conn;
+
     log('WebSocket Client Connected');
-    connection.on('error', function(error) {
+    conn.on('error', function(error) {
         log('Connection Error: ' + error.toString());
     });
-    connection.on('close', function() {
+    conn.on('close', function() {
         log('Connection Closed');
     });
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            log("Received: '" + message.utf8Data + "'");
+    conn.on('message', function(msg) {
+        if (msg.type === 'utf8') {
+            log("Received: '" + msg.utf8Data + "'");
         }
-    });
 
-    function sendNumber() {
-        if (connection.connected) {
-            var number = Math.round(Math.random() * 0xFFFFFF);
-            connection.sendUTF(number.toString());
-            setTimeout(sendNumber, 1000);
+        const message = JSON.parse(msg.utf8Data);
+
+        if (message.state === 'info') {
+            do_login();
         }
-    }
-    // sendNumber();
+
+        if (message.state === 'start') {
+            games[message.game] = message;
+            send({game:message.game,state:"move",strategy:1});
+        }
+
+        if (message.state === 'turnover') {
+            const game = games[message.game];
+            const opponents_move = message.moves[(game.hand + 1) % 2];
+
+            send({game:message.game,state:"move",strategy:opponents_move});
+        }
+
+        if (message.state === 'gameover') {
+            console.log(message.game + ' is over');
+        }
+
+    });
 });
 
 client.connect('ws://dmc.alepoydes.com:3012');
