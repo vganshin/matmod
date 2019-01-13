76d2db7a0ce40baa7836b69dd1c52ee36cc437af
Vladislav Ganshin <ganshinv@gmail.com>
2019-01-14 00:18:48 +0300

    Add rating_saver.bash

---
+++ rating_saver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+http http://dmc.alepoydes.com/score/16/release > current.html
+if `cmp -s current.html last.html`
+then
+  echo "The same."
+else
+  echo "Rating updated."
+  cp current.html last.html
+  cp last.html "`date`.html"
+fi
