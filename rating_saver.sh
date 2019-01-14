0cb11938d4b4a7654e3a760f936384d95fbde126
Vladislav Ganshin <ganshinv@gmail.com>
2019-01-15 02:07:47 +0300

    Update rating saver

--- rating_saver.sh
+++ rating_saver.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-http http://dmc.alepoydes.com/score/16/release > current.html
-if `cmp -s current.html last.html`
-then
-  echo "The same."
-else
-  echo "Rating updated."
-  cp current.html last.html
-  cp last.html "`date +%Y/%m/%d-%H:%M.html`"
-fi
+echo `date` >> ~/last_run.txt
+
+function save {
+  id=$1
+  mode=$2
+  name=$3
+
+  dirname=$name-$mode
+
+  mkdir -p $dirname
+  touch $dirname/last.html
+
+  http http://dmc.alepoydes.com/score/$id/$mode > $dirname/current.html
+  if `cmp -s $dirname/current.html $dirname/last.html`
+  then
+    echo "The same."
+  else
+    echo "Rating updated."
+    cp $dirname/current.html $dirname/last.html
+    cp $dirname/last.html "$dirname/`date +%Y-%m-%d-%H:%M.html`"
+  fi
+  echo Success for $id $mode $name
+}
+
+save 16 release mnr
+save 16 debug mnr
