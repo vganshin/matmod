088e8e7b4c35819f97d6a71c341db83cc7a036d9
Vladislav Ganshin <ganshinv@gmail.com>
2019-01-28 00:47:44 +0300

    Fix prisoner's strategy

--- src/prisoner/index.js
+++ src/prisoner/index.js
@@ -1,6 +1,9 @@
 const helpers = require('../helpers');
 
 function rootStrategy(game) {
+  if (game.moves.length === 0) {
+    return 1;
+  }
   if (helpers.isOpponentMirror(game)) {
     return 1;
   }
